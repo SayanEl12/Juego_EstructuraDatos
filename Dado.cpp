b474d0dc706ca9d32d3df0f4ddf435c4a4f45944
felimontyr <166776537+felimontyr@users.noreply.github.com>
2024-04-15 19:17:38 -0500

    Update Dado.cpp

--- Dado.cpp
+++ Dado.cpp
@@ -1,10 +1,17 @@
+#include <cstdlib> 
+#include <ctime>
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 class Dado{
     public:
     int dados[4];
-
-    Dado: lanzamientos(0) (){
+    int lanzamientos;
+    
+    Dado(): lanzamientos(0){
         srand(time(0));
-        roll();
+        //roll();
     }
     
     int contLanzadas() {
@@ -24,18 +31,20 @@ class Dado{
     }
     
     vector<int> retornarPares(){
-        for(int i = 0;i<4;i++){
+        
+        for(int i = 0;i<i;i++){
             cout<<dados[i]<<endl;
         }
+        
         int r1, r2, r3, r4;
         r1 = dados[0];
         r2 = dados[1];
         r3 = dados[2];
         r4 = dados[3];
         
-        string choice;
-        cout<<"con cual resultado quiere emparejar el resultado 1 :"<<endl;
-        cin>>choise;
+        string choise;
+        cout << "con cual resultado quiere emparejar el resultado 1: " << endl;
+        cin >> choise;
         
         int par1, par2;
         if(choise == "r2"){
@@ -52,12 +61,31 @@ class Dado{
         cout<<"par 1: "<<par1<<endl;
         cout<<"par 2: "<<par2<<endl;
         
-        vector<int> parDados[2] = {par1, par2};
+        vector<int> parDados;
+        parDados.push_back(par1);
+        parDados.push_back(par2);
         
         return parDados;
         
     }
-    int contLanzadas(){
-        return lanzamientos;
-    }
+    
 };
+
+class Ficha{
+
+    
+};
+
+int main(){
+    
+    Dado dado;
+    
+    dado.lanzarDados();
+    dado.mostrarDados();
+    dado.retornarPares();
+    
+    
+    
+    
+    
+}
